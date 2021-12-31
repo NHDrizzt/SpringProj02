81d8498ddd2ebfb0fb658ba9ac7d12757582adad
Rokol <biel_s.carvalho2@hotmail.com>
2021-12-31 20:34:08 -0300

    projecao de dados do autor com dto

--- src/main/java/com/nhdrizzit/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nhdrizzit/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nhdrizzit.workshopmongo.domain.Post;
 import com.nhdrizzit.workshopmongo.domain.User;
+import com.nhdrizzit.workshopmongo.dto.AuthorDto;
 import com.nhdrizzit.workshopmongo.repository.PostRepository;
 import com.nhdrizzit.workshopmongo.repository.UserRepository;
 
@@ -29,15 +30,17 @@ public class Instantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User jake = new User(null, "Jake", "jake@gmail.com");
 		User dude = new User(null, "Dude", "dude@gmail.com");
 		User lake = new User(null, "Lake", "lake@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("31/12/2021"), "Partiu!", "Viajem pra SP", jake);
-		Post post2 = new Post(null, sdf.parse("05/01/2022"), "Tamo aí", "Yes", jake);
-		
 		userRepository.saveAll(Arrays.asList(jake,dude,lake));
+		
+		Post post1 = new Post(null, sdf.parse("31/12/2021"), "Partiu!", "Viajem pra SP", new AuthorDto(jake));
+		Post post2 = new Post(null, sdf.parse("05/01/2022"), "Tamo aí", "Yes", new AuthorDto(jake));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
