1f535179a91f477ac1687d818e1d8330a0bec13d
Rokol <biel_s.carvalho2@hotmail.com>
2022-01-01 22:04:56 -0300

    Ref posts to user

--- src/main/java/com/nhdrizzit/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nhdrizzit/workshopmongo/config/Instantiation.java
@@ -43,5 +43,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		jake.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(jake);
+		
 	}
 }
